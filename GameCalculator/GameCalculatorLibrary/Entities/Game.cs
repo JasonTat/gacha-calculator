2b610e35b77277f3895479063c85a83cc3391968
JasonTat <112578835+JasonTat@users.noreply.github.com>
2024-05-09 17:21:51 -0600

    app + db created

---
+++ GameCalculator/GameCalculatorLibrary/Entities/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace GameCalculatorLibrary.Entities
+{
+    public class Game
+    {
+        public int ID { get; set; }
+        public string GameName { get; set; }    
+        public int EnergyRegenRate { get; set; }
+        public int MaximumEnergyCap { get;set; }
+    }
+}
