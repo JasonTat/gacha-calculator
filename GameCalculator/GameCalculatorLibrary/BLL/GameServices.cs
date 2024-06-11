71560ba52aa179597c0af7bcf9bf8b8ecaf3a17e
JasonTat <112578835+JasonTat@users.noreply.github.com>
2024-06-11 12:58:00 -0600

    Created PopulateGameList method, added methods to GameService.cs, updated property name on Game class

--- GameCalculator/GameCalculatorLibrary/BLL/GameServices.cs
+++ GameCalculator/GameCalculatorLibrary/BLL/GameServices.cs
@@ -16,5 +16,47 @@ namespace GameCalculatorLibrary.BLL
             _gameCalculatorContext = gameCalculatorContext;
         }
 
+        #region query
+        public Game FetchGameFromId(int id)
+        {
+            Game game = _gameCalculatorContext.Games
+                            .Where(x => x.ID == id)
+                            .Select(x => new Game
+                            {
+                                ID = x.ID,
+                                GameName = x.GameName,
+                                MinutesPerEnergy = x.MinutesPerEnergy,
+                                MaximumEnergyCap = x.MaximumEnergyCap
+                            })
+                            .FirstOrDefault();
+            return game;
+        }
+
+        public int GameEnergyCalculator(int currentAmount, Game game)
+        {
+            int totalTimeToReplenish;
+            int difference = (int)game.MaximumEnergyCap - currentAmount;
+            totalTimeToReplenish = difference * (int)game.MinutesPerEnergy;
+            return totalTimeToReplenish;
+        }
+
+        public List<Game> PopulateGameList()
+        {
+            List<Game> list = new List<Game>();
+
+            list = _gameCalculatorContext.Games
+                        .Select(x => new Game
+                        {
+                            ID = x.ID,
+                            GameName = x.GameName,
+                            MinutesPerEnergy = x.MinutesPerEnergy == null ? 0 : (int)x.MinutesPerEnergy,
+                            MaximumEnergyCap = x.MaximumEnergyCap == null ? 0 : (int)x.MaximumEnergyCap
+                        })
+                        .ToList();
+            return list;
+        }
+
+        #endregion
+
     }
 }
--- GameCalculator/GameCalculatorLibrary/Entities/Game.cs
+++ GameCalculator/GameCalculatorLibrary/Entities/Game.cs
@@ -4,7 +4,7 @@
     {
         public int ID { get; set; }
         public string GameName { get; set; }    
-        public int EnergyRegenRate { get; set; }
+        public int MinutesPerEnergy { get; set; }
         public int MaximumEnergyCap { get;set; }
     }
 }
