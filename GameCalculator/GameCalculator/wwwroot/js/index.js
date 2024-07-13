630ef1b809109c6b0a6a5db94a59460d70a84f38
JasonTat <112578835+JasonTat@users.noreply.github.com>
2024-07-12 19:46:28 -0600

    included background image, hover/blue effect on main div.

---
+++ GameCalculator/GameCalculator/wwwroot/js/index.js
@@ -0,0 +1,10 @@
+var gameName = document.getElementById("game-name");
+var body = document.getElementsByTagName('body')
+
+if (gameName.innerText == "Honkai Star Rail") {
+
+    body.classList.add('star-rail-background');
+
+}
+
+console.log(gameName);
